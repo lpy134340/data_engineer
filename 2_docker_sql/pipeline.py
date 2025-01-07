f64dc0eda3db6efbc99fd72722c4cb80f9af0013
lpy134340 <lpy134340@gmail.com>
2025-01-06 21:34:13 -0500

    added docker intro

---
+++ 2_docker_sql/pipeline.py
@@ -0,0 +1,14 @@
+import sys
+import pandas as pd
+
+# test stuff with pandas
+
+print(sys.argv)
+
+# name of the file
+# sys.argv[0]
+
+# the input
+day = sys.argv[1]
+
+print(f'job finished successfully for day = {day}')
